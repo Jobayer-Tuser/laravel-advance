6178717277c6d079c10a24473b7467b517f3ac9d
Jobayer Al Mahmud Tuser <jtuser112@gmail.com>
2021-01-14 16:02:26 +0600

    Data seeding in laravel

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory\ as Faker;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //after making this class run : composer dump-autoload in your command line
+        DB::table('posts')->insert([
+            'title' => 'This is the title',
+            'description' => 'This is the description of the table',
+        ]);//use for cusomise seeding data
+        
+        $faker = Faker::create();
+        foreach(range(1, 20) as $index){
+            DB::table('posts')->insert([
+                'title' => $faker->sentence(5),
+                'description' => $faker->paragraph(4);
+            ]);//use for fake seeding data
+        }
+    }
+}
